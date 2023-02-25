b94279fa31e6202c61576c4d44a3d8aa6448fac7
Johan <jhoan_estiven2@hotmail.es>
2023-02-25 15:06:44 -0500

    ProjectToCitiesAndEstates

---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿    using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caractéres")]
+        public string Name { get; set; } = null!;
+
+        public ICollection<State>? States { get; set; }
+
+        public int StatesNumber => States == null ? 0 : States.Count;
+    }
+}
